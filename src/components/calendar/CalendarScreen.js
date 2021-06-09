32e34a4dee5458e3cf4e0394e06ddb660e927ec8
pacomarm <francisco.marmolejoml@udlap.mx>
2021-06-09 17:37:24 -0500

    calendar setup

--- src/components/calendar/CalendarScreen.js
+++ src/components/calendar/CalendarScreen.js
@@ -1,9 +1,31 @@
 import React from 'react'
+import { Calendar, momentLocalizer } from 'react-big-calendar'
+import moment from 'moment'
 import {NavBar} from '../ui/NavBar'
+
+import 'react-big-calendar/lib/css/react-big-calendar.css';
+
+const localizer = momentLocalizer(moment);
+const events = [{
+    title: 'My bd',
+    start: moment().toDate(),
+    end: moment().add(2, 'hours').toDate(),
+    bgcolor: '#fafafa'
+}]
+
+
 export const CalendarScreen = () => {
     return (
         <div>
             <NavBar/>
+
+            <Calendar
+                localizer={localizer}
+                events={events}
+                startAccessor="start"
+                endAccessor="end"
+                style={{ height: '100vh' }}
+            />
         </div>
     )
 }
--- src/components/ui/NavBar.js
+++ src/components/ui/NavBar.js
@@ -5,7 +5,7 @@ export const NavBar = () => {
         <div className="navbar navbar-dark bg-dark mb-4">
             <span className="navbar-brand">Paco</span>
             <button className="btn btn-outline-danger">
-                Salir
+                Exit
                 <i className=" ml-2 fas fa-sign-out-alt"></i>
             </button>
         </div>
