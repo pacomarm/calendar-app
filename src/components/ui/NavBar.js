428930f372cd35b2d1787036e6b518d5f89c501b
pacomarm <francisco.marmolejoml@udlap.mx>
2021-06-13 00:06:50 -0500

    logout be done

--- src/components/ui/NavBar.js
+++ src/components/ui/NavBar.js
@@ -1,10 +1,23 @@
 import React from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { startLogout } from '../../actions/auth';
 
 export const NavBar = () => {
+
+    const {name} = useSelector( state => state.auth );
+    const dispatch = useDispatch();
+
+    const handleLogout = () => {
+        dispatch( startLogout() )
+    }
+
     return (
         <div className="navbar navbar-dark bg-dark mb-4">
-            <span className="navbar-brand">Paco</span>
-            <button className="btn btn-outline-danger">
+            <span className="navbar-brand">{name}</span>
+            <button 
+                className="btn btn-outline-danger"
+                onClick={handleLogout}
+            >
                 Exit
                 <i className=" ml-2 fas fa-sign-out-alt"></i>
             </button>
